2c9a2fddd8f72b9835f0ef622cc99862d0dbd41c
Jazoub2 <jakrem@edu.umea.se>
2023-10-18 09:08:37 +0200

    klar med startupg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,63 @@
 
 <h1>
-  Välkommen till min hemsida!
+  Abandon every hope all ye who enter here!
 </h1>
 
-<h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+<h2>Beware, intrepid traveler, for you have stepped beyond the threshold of mortal lands. Abandon all hope, ye who enters here, for the River Styx flows deep and the soul's echo is the only sound that lingers.
+  Tread cautiously, for each step leads you further into the abyss, where the forgotten and the forsaken find their eternal abode. In this forsaken realm, even the bravest heart may falter, and the strongest will may shatter like glass upon the stones of fate.
+  Darkness veils your path, and the cries of lost souls pierce the silence. Turn back now, for here in Hades, the price of curiosity may be paid in tears and endless sorrow."
+</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 class="Name">In this realm of perpetual twilight, the boundary between reality and nightmare blurs. Your presence here is a defiance of mortality itself, and with every step, you venture deeper into a realm where the past, present, and future converge in a spectral dance.
+  Will you find the answers you seek, or become just another whisper in the haunting chorus of the dead? The choice is yours, though the consequences may be as eternal as the shadows that surround you.</h3>
+
+<h4>
+  "ZORK AND THE GREAT UNDERGROUND EMPIRE ENDING"
+</h4>
+
+<p>
+  "As you enter the barrow, the door closes inexorably behind you. Around you it is dark, but ahead is an enormous cavern, brightly lit.
+  Through its center runs a wide stream. Spanning the stream is a small wooden footbridge, and beyond a path leads into a dark tunnel. Above the bridge,
+  floating in the air, is a large sign. It reads: All ye who stand before this bridge have completed a great and perilous adventure witch has tested your wit
+  and courage. You have mastered the first part of the Zork trilogy. Those who pass over this bridge must be prepared to undertake an even greater adventure that will severely
+  test your skill and bravery!"
+
+</p>
+
+
+
+
+<style>
+  h1,h2,h3,h4,p{
+  display: inline-block;
+  color:rgb(0, 0, 0);
+  font-size: 95%;
+  font-family: 'Courier New', Courier, monospace;
+}
+  
+
+/*h2{
+  color:rgb(0, 0, 0);
+  font-family: 'Courier New', Courier, monospace;
+ 
+}
+h3{
+  color:rgb(0, 0, 0);
+  font-size: 150%;
+}*/
+/*#header{
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 160%;
+  }*/
+
+  /*#bread{
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 140%;
+  }*/
+
+/*style ="background-color:pink" id="bread"*/
+</style>
+
+
+
 
